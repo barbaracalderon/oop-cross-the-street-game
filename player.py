779b9b4a4d2f2cf539e65612f7b813aadd5a1fe5
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-05-05 17:22:32 -0300

    Added turtle to screen and made it move upwards on command

--- player.py
+++ player.py
@@ -2,6 +2,18 @@ STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
+from turtle import Turtle
 
-class Player:
-    pass
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("green")
+        self.shape("turtle")
+        self.penup()
+        self.left(90)
+        self.goto(STARTING_POSITION)
+
+    def move_up(self):
+        new_y = self.ycor() + 15
+        self.goto(self.xcor(), new_y)
