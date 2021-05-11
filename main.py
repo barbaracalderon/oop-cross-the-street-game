798db2203eb56bdbe44934f3359779d9b3d4f28d
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-05-11 02:09:16 -0300

    Created random cars moving at a certain pace

--- main.py
+++ main.py
@@ -13,8 +13,11 @@ player = Player()
 screen.listen()
 screen.onkey(fun=player.move_up, key="Up")
 
+car_manager = CarManager()
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-
+    car_manager.create_car()
+    car_manager.move_cars()
