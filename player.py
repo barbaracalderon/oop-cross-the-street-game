c913d2d4ffda466eb8c6a61ea2c929699c1b7202
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-05-11 01:42:23 -0300

    Simplified the method to move forward

--- player.py
+++ player.py
@@ -15,5 +15,4 @@ class Player(Turtle):
         self.goto(STARTING_POSITION)
 
     def move_up(self):
-        new_y = self.ycor() + 15
-        self.goto(self.xcor(), new_y)
+        self.forward(MOVE_DISTANCE)
