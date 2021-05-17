e42a91dd10f67bd9cf8004e38d326165591636df
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-05-17 13:56:07 -0300

    Scoreboard now updates itself according to level

--- scoreboard.py
+++ scoreboard.py
@@ -9,5 +9,16 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.penup()
         self.level = 1
+        self.update_scoreboard()
+
+
+
+    def update_scoreboard(self):
+        self.clear()
         self.goto(-280, 260)
         self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
+
